63f4570f2459bea2d5557b0fb316d0a61a6e073d
AveryOn <lnkonion2@gmail.com>
2025-04-24 10:32:06 +0500

    enable cors

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
+
+// Включаем CORS для всех источников
+app.use(cors({
+    origin: 'http://localhost:4556',
+}));
 
 app.get('/ping', (req, res) => {
   res.send('Hello World!');
