5664e5f41de0063c2619d7714971b9a4ff561f73
AveryOn <lnkonion2@gmail.com>
2025-05-04 00:31:25 +0500

    upd server,ts

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,26 @@
 import express from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config()
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080'
+
 
 // Включаем CORS для всех источников
 app.use(cors({
-    origin: 'http://localhost:4556',
+  origin: CLIENT_URL,
 }));
 
 app.get('/ping', (req, res) => {
-  res.send('Hello World! (UPD)123');
+  res.send('Hello World!as');
 });
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.get('/api/v2/ping', (req, res) => {
+  res.send('[PING] V2');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
