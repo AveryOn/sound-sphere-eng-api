9cb36c1bd4065fed88d89d2a9b62d888e747d0f5
AveryOn <lnkonion2@gmail.com>
2025-05-03 22:28:05 +0500

    cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,17 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:8080'
 
 // Включаем CORS для всех источников
 app.use(cors({
-    origin: '*',
+  origin: [
+    'https://dev.sound-sphere-eng.xyz-avery.online',
+    'https://sound-sphere-eng.xyz-avery.online',
+    'http://localhost:4555',
+    'http://localhost:4556',
+    'http://localhost:4552',
+    'http://localhost:4553',
+    'http://localhost:4554',
+    'http://localhost:4557',
+    'http://localhost:4558',
+  ],
 }));
 
 app.get('/ping', (req, res) => {
